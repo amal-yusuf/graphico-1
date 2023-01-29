e9f5cff8938c765e4a3adcb441d15a1ba01919a3
amal-yusuf <amal.yusuf@hotmail.co.uk>
2023-01-29 21:24:24 +0000

    Added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -34,7 +34,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_GRAPHICS_THRESHOLD:
             self.graphics_cost = self.order_total * settings.STANDARD_GRAPHICS_PERCENTAGE / 100
         else:
