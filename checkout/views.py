4c3383ee05512114a4666c09aabe444964dc7c8b
amal-yusuf <amal.yusuf@hotmail.co.uk>
2023-01-29 20:00:52 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Lz3N7GcnROZLgz31xnLgi9o9sH6wgFbqVeGS2nB0TKrIC1gZw3FePj2weupYWdmePvj840c0cGl1vuWNDWJy0Cf00NCn6ArJQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
